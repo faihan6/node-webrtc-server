f3b978f99c83bf34b083acb8cb1c2fb8c9f62025
faihan6 <faihanali111@gmail.com>
2025-05-26 19:08:03 +0530

    1. Removed toml as a dependency. 2. Config files are now JS files. 3. Auto generate server-config on `npm install` 4. Improvements in .gitignore 5. Removed unnecessary log file

---
+++ example-server-config.js
@@ -0,0 +1,34 @@
+
+const config = {
+
+    // Certificate and Key must be provided here. We will not generate them.
+    certificatePath: "cert.pem",
+    keyPath: "key.pem",
+
+    // Specify the audio codecs that will be supported.
+    audioSupportedCodecs: ["opus/48000"],
+
+    // Specify the video codecs that will be supported.
+    videoSupportedCodecs: ["VP8/90000"],
+
+    // Specify the header extensions that will be supported.
+    supportedHeaderExtensions: [
+        'urn:ietf:params:rtp-hdrext:sdes:mid'
+    ],
+
+    // Specify the IP address that will be used as the IP address in SRFLX candidates.
+    //publicIP: "0.0.0.0",
+
+    // Specify the time (in milliseconds) to wait before sending NACKs.
+    nackWaitTimeMS: 0,
+
+    // Specify the path to the file where DTLS secrets are logged.
+    // keyLogOutputPath: "keylog.log",
+
+    // Disable DTLS encryption. STRICTLY FOR DEBUGGING PURPOSES ONLY.
+    disableWebRTCEncryption: false,
+}
+
+
+module.exports = config;
+
